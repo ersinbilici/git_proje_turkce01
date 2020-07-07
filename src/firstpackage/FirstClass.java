8da5a466083259a6e78bd55f88f8b3bfbc1142bd
ersinbilici <67049444+ersinbilici@users.noreply.github.com>
2020-07-07 11:32:44 +0200

    conflict

--- src/firstpackage/FirstClass.java
+++ src/firstpackage/FirstClass.java
@@ -4,9 +4,9 @@ public class FirstClass {
 	public static void main(String[] args) {
 		
 		System.out.println("first commit from local repository");
-		System.out.println("second commit from local repository");
-		System.out.println("conflict yapmak istiyoruz");
-		System.out.println("first commit from remote repository");
+		System.out.println("ikinci commit from local repository");
+		System.out.println("conflict nedir?");
+		System.out.println("adana köprü basi");
 		
 
 	}
