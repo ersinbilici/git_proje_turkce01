ffcc8fd5b78f583e517a381ac89806e86e7f330a
ersinbilici <67049444+ersinbilici@users.noreply.github.com>
2020-07-07 10:56:59 +0200

    second commit from local repository

--- src/firstpackage/FirstClass.java
+++ src/firstpackage/FirstClass.java
@@ -4,6 +4,7 @@ public class FirstClass {
 	public static void main(String[] args) {
 		
 		System.out.println("first commit from local repository");
+		System.out.println("second commit from local repository");
 		
 
 	}
