44918cc4979b373ded4569092f0dac7aa9fd0ba4
ersinbilici <67049444+ersinbilici@users.noreply.github.com>
2020-07-07 11:30:59 +0200

    Update FirstClass.java
    ikinci line

--- src/firstpackage/FirstClass.java
+++ src/firstpackage/FirstClass.java
@@ -4,7 +4,7 @@ public class FirstClass {
 	public static void main(String[] args) {
 		
 		System.out.println("first commit from local repository");
-		System.out.println("second commit from local repository");
+		System.out.println("ikincide de degisiklik yaptim remoteden");
 		System.out.println("conflict yapmak icin cok ugrastim ama olmadi mi yoksa?");
 		System.out.println("bir de burda sorun olsun");
 		
