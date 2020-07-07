ee0f78993cfc48748705d433d23719081cbbfe82
ersinbilici <67049444+ersinbilici@users.noreply.github.com>
2020-07-07 11:13:16 +0200

    Update FirstClass.java
    first commit from remote repository

--- src/firstpackage/FirstClass.java
+++ src/firstpackage/FirstClass.java
@@ -6,6 +6,7 @@ public class FirstClass {
 		System.out.println("first commit from local repository");
 		System.out.println("second commit from local repository");
 		System.out.println("third commit from local repository");
+		System.out.println("first commit from remote repository");
 		
 
 	}
